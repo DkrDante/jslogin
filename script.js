609ba129e98feb8547cfaa36f73b10418231f917
Dante <dkrdante@gmail.com>
2024-02-25 23:12:48 +0530

    wesite made using html,js,css

---
+++ script.js
@@ -0,0 +1,21 @@
+const wrapper = document.querySelector('.wrapper');
+const registerlink = document.querySelector('.register-now');
+const loginlink = document.querySelector('.login-now');
+const btnpop = document.querySelector('.butlogpop');
+const close = document.querySelector('.close')
+registerlink.onclick = () => {
+    wrapper.classList.add('active');
+}
+loginlink.onclick = () => {
+    wrapper.classList.remove('active');
+}
+btnpop.onclick = () => {
+    wrapper.classList.add('active-popup');
+
+}
+close.onclick = () => {
+    wrapper.classList.remove('active-popup');
+    wrapper.classList.remove('active');
+
+
+}
